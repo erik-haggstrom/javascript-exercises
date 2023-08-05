eef638277e908cdc6df6bf7cf30c9e617dcce3c6
erik-haggstrom <erik.haggstrom0@gmail.com>
2023-08-05 21:10:22 +0200

    Completed the third exercise I am too good

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
-};
+function reverseString(wordInput) {
+let wordReversed = "";
+for (let i = wordInput.length - 1; i >= 0; i--){
+  wordReversed += wordInput.charAt(i);
+}
+return wordReversed;
+}
 
 // Do not edit below this line
 module.exports = reverseString;
