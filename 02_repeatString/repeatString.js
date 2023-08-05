629a3039bb98e7e53f7c1b43c485838f4803354f
erik-haggstrom <erik.haggstrom0@gmail.com>
2023-08-05 19:58:15 +0200

    completed the second exersice

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
-};
+function repeatString(string, num) {
+  if (num < 0){
+    return "ERROR"
+  }
+  else{
+    return string.repeat(num);
+  }
+  
+}
 
 // Do not edit below this line
 module.exports = repeatString;
