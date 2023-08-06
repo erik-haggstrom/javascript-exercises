f025cd65e8c72ffbf8752878e85bc8c792ccf565
erik-haggstrom <erik.haggstrom0@gmail.com>
2023-08-06 22:51:51 +0200

    Completed the fith exercise took a little thinking but got it

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
+function sumAll(x, y) {
+  let sum = 0;
+  if ((typeof x === 'number' && x > 0) && (typeof y === 'number' && y > 0)){
+    if (x < y){
+      for (let i = 0; i <= (y - x); i++){
+        sum += x + i;
+      }
+    }
+    else if (x > y){
+      for (let i = 0; i <= (x - y); i++){
+        sum += x - i;
+      }
+    }
+  }
+  else{
+    sum = "ERROR"
+  }
+  return sum;
+}
 
-};
+
 
 // Do not edit below this line
 module.exports = sumAll;
