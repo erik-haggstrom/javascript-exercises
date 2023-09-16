c463ae78d5f33cbbe2598cb4845318296a2d667d
erik-haggstrom <erik.haggstrom0@gmail.com>
2023-09-16 13:25:26 +0200

    completed the calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let value = array.reduce((acc, current) => {
+    return acc + current;
+  }
+  ,0);
+  return value;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let value = array.reduce((acc, currentV) => {
+    return acc * currentV;
+  });
+  return value;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let value = 1;
+  if (a<1) {
+    let value = 1;
+  }
+  
+  for (let i = 0; a>i;){
+    value *= a;
+    a--;
+  }
+
+  return value;
 };
 
 // Do not edit below this line
