d1b6172c021e231deb2ca81002c8dd0eac924c9d
erik-haggstrom <erik.haggstrom0@gmail.com>
2023-09-16 16:48:27 +0200

    Completed this exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  let answer = false;
+  let newString = '';
+  for (let i = (string.length - 1); i>=0; i--) {
+    newString += string[i];
+  }
+  let lowerString = string.toLocaleLowerCase();
+  let checkString = lowerString.split(/[! ,.]+/);
+  let testString = checkString.join('');
 
+  let lowerNewstring = newString.toLocaleLowerCase();
+  let words = lowerNewstring.split(/[! ,.]+/);
+  let finalString = words.join('');
+
+  if (testString === finalString) {
+    answer = true;
+  }
+  return answer;
 };
 
 // Do not edit below this line
