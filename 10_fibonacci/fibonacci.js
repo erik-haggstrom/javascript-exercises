ec592eb0546f2de205e2eef6eb37b6694f9c913d
erik-haggstrom <erik.haggstrom0@gmail.com>
2023-09-16 18:44:03 +0200

    Completed this exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+  if (n<0) {
+    return 'OOPS'
+  }
+  let N = Number(n);
+  
+  let list = [];
+  list[0] = 0;
+  list[1] = 1;
 
+  for (let i = 2; i <=N; i++) {
+    list[i] = list[i-1] + list[i-2];
+  }
+  return list[N];
 };
 
 // Do not edit below this line
